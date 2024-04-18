154d57a39d07b5364a0451e2ff9a742309263d2a
kiddmetro <igweclintonmatro@gmail.com>
2024-04-18 16:58:07 +0100

    complete level 2

--- 02Excercise/Level2/level2.js
+++ 02Excercise/Level2/level2.js
@@ -50,6 +50,17 @@ console.log(rand.charAt(randomNum));
 console.log(rand.charAt(3));
 console.log(randomNum);
 
+// QUESTION 11
+console.log('1 1 1 1 1 \n2 1 2 4 8 \n3 1 3 9 27 \n4 1 4 16 64 \n5 1 5 25 125 ');
+
+// QUESTION 12
+let wordings = "You cannot end a sentence with because because because is a conjunction";
+let wordSliceOne = wordings.substr(0, 30);
+let wordSliceTwo = wordings.substr(54);
+console.log(wordSliceOne + wordSliceTwo);
+
+
+
 
 
 
