fe4bd1c26c92217cfbe46b5701e19cf2981582e8
kiddmetro <igweclintonmatro@gmail.com>
2024-04-19 10:57:33 +0100

    initial commit

---
+++ 03Excercise/Level1/level1.js
@@ -0,0 +1,15 @@
+// QUESTION 1
+const firstName = 'Ahmed';
+const lastName = "Sule";
+const country = 'Egypt';
+const city = 'Cairo';
+const age = 34;
+const isMarried = false;
+const year = 2020;
+
+console.log(typeof firstName, typeof lastName, typeof country, typeof city, typeof age, typeof isMarried, typeof year);
+
+// QUESTION 2
+const stringNum = '10';
+const number = 10;
+console.log(stringNum == number);
