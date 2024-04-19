1d4b67a0f22cf027416bf92e3f4ffeb4633a4fd6
kiddmetro <igweclintonmatro@gmail.com>
2024-04-19 13:16:08 +0100

    initial commit

---
+++ 03Excercise/Level2/level2.js
@@ -0,0 +1,57 @@
+// QUESTION 1
+// const base = prompt("Enter Base");
+// const height = prompt("Enter Height");
+
+// const calculateArea = 0.5 * base * height;
+
+// alert(`The Area of the Triangle is ${calculateArea}`);
+
+// QUESTION 2
+/*const sideA = prompt("Enter Side A");
+const sideB = prompt("Enter Side B");
+const sideC = prompt("Enter Side C");
+
+const convertInputA = Number(sideA);
+const convertInputB = Number(sideB);
+const convertInputC = Number(sideC);
+
+
+const calculatePerimeter = convertInputA + convertInputB + convertInputC;
+alert(`The Perimeter of the triangle is ${calculatePerimeter}`); */
+
+// QUESTION 3
+
+/*const length = prompt("Enter Length");
+const width = prompt("Enter Width");
+
+calculateArea = length * width;
+const convertLength = Number(length);
+const convertWidth = Number(width);
+calculatePerimeter = 2 * (convertLength + convertWidth);
+
+alert(`The Area of the Rectangle is ${calculateArea} and perimeter ${calculatePerimeter}`);*/
+
+// QUESTION 4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
