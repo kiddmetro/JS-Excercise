fdcafbce31748284c3a6dd239e0dc956f02510b6
kiddmetro <igweclintonmatro@gmail.com>
2024-04-19 21:13:51 +0100

    initial commit

--- 03Excercise/Level2/level2.js
+++ 03Excercise/Level2/level2.js
@@ -32,15 +32,77 @@ calculatePerimeter = 2 * (convertLength + convertWidth);
 alert(`The Area of the Rectangle is ${calculateArea} and perimeter ${calculatePerimeter}`);*/
 
 // QUESTION 4
+// const radius = prompt("Enter Radius");
+// let PI = 3.14
+// const area = PI * radius * radius;
+// const circumference = 2 * PI * radius;
+// alert(`The Area of a circle is ${area} and circumference is ${circumference}`);
 
+// QUESTION 5
+/*
+// Find x-intercept
+let X1 = 0;
+let Y1= 2 * X1 - 2;
+console.log(`x-intercept is (${X1},${Y1})`);
 
+// // Find y-intercept
+let Y2= 0;
+let X2 = 2/2;
+console.log(`y-intercept is (${X2},${Y2})`);
 
+// Find Slope
+const m1 = (Y2 - Y1) / (X2 - X1);
+console.log(`The Slope is ${m1}`);
+*/
 
+// QUESTION 6
 
+/*let x1 =2;
+let y1 = 2;
+let x2 = 6;
+let y2 = 10;
 
+let m2 = (y2 - y1) / (x2 - x1);
+console.log(`The Slope is ${m2}`); */
 
+// QUESTION 7
 
+// console.log(m1 == m2);
+// console.log(m1 >= m2);
+// console.log(m1 === m2);
 
+// QUESTION 8
+
+let x = 1;
+let y = x * 2 + 6 * x + 9;
+console.log(y);
+
+// Did Find Any 
+
+// QUESTION 9
+
+const hours = prompt("Enter Hours");
+const emplyHours = prompt("Enter Rate Per Hour");
+
+const weekEarn = hours * emplyHours;
+
+alert(`Your weekly earning is ${weekEarn}`);
+
+// QUESTION 10
+
+const name = "clinton";
+
+let lenName = name.lenght;
+lenName > 7 ? console.log("Name is too Long") : console.log("Name is too Short");
+
+// QUESTION 11
+
+let firstName = "Clinton";
+let lastName = "Igwe"
+
+let lenFirstName = firstName.length;
+let lenLastName = lastName.length;
+lenFirstName > lenLastName ? console.log(`Your first name, ${firstName} is Longer than your family name ${lastName}`) : console.log(`Your first name, ${firstName} is shorter than your family name ${lastName}`);
 
 
 
