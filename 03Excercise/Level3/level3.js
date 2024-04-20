b61165ee96c9102e81a84e486e82ff9af38dac29
kiddmetro <igweclintonmatro@gmail.com>
2024-04-20 16:40:41 +0100

    level 3 incomplete

---
+++ 03Excercise/Level3/level3.js
@@ -0,0 +1,7 @@
+let year = new Date().getFullYear();
+let month = new Date().getMonth();
+let date = new Date().getDate();
+let hour = new Date().getHours();
+let minutes = new Date().getMinutes();
+
+console.log(`${year}-${month}-${date} ${hour}:${minutes}`);
