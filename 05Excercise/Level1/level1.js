20cdc0e551a2d80f415e878a5e5881d8a1e3d08b
kiddmetro <igweclintonmatro@gmail.com>
2024-04-21 19:11:11 +0100

    initial Commit

--- 05Excercise/Level1/level1.js
+++ 05Excercise/Level1/level1.js
@@ -22,3 +22,44 @@ const itCompaines = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle'
 // QUESTION 7
 console.log(itCompaines);
 
+// QUESTION 8
+console.log(itCompaines.length);
+
+// QUESTION 9
+console.log(itCompaines[0]);
+console.log(itCompaines[3]);
+console.log(itCompaines[6]);
+
+// QUESTION 10
+console.log(itCompaines[0]);
+console.log(itCompaines[1]);
+console.log(itCompaines[2]);
+console.log(itCompaines[3]);
+console.log(itCompaines[4]);
+console.log(itCompaines[5]);
+console.log(itCompaines[6]);
+
+// QUESTION 11
+console.log(itCompaines[0].toUpperCase());
+console.log(itCompaines[1].toUpperCase());
+console.log(itCompaines[2].toUpperCase());
+console.log(itCompaines[3].toUpperCase());
+console.log(itCompaines[4].toUpperCase());
+console.log(itCompaines[5].toUpperCase());
+console.log(itCompaines[6].toUpperCase());
+
+// QUESTION 12
+console.log(itCompaines.toString());
+
+// QUESTION 13
+let anyCompany = 'ice';
+if(itCompaines.includes(anyCompany)){
+    console.log(anyCompany)
+}
+else{
+    console.log("Company is Not Found")
+}
+
+// OR
+
+// const myCompany 
