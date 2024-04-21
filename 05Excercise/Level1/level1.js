403e1a5c1431715588ea26a9e58893bb0163be41
kiddmetro <igweclintonmatro@gmail.com>
2024-04-21 18:36:06 +0100

    initial Commit

---
+++ 05Excercise/Level1/level1.js
@@ -0,0 +1,24 @@
+// QUESTION 1
+const empArr = [];
+
+// QUESTION 2
+const eleArr = ['Hello', 'Morning', true , 24, 'Mind'];
+
+//QUESTION 3
+console.log(eleArr.length);
+
+// QUESTION 4
+console.log(eleArr[0]);
+console.log(eleArr[2]);
+console.log(eleArr[4]);
+
+// QUESTION 5
+const diffArr = ['34', true, false, 34, 'Esxpae', 5];
+console.log(diffArr.length);
+
+// QUESTION 6
+const itCompaines = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'];
+
+// QUESTION 7
+console.log(itCompaines);
+
