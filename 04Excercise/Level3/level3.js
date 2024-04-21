9d7c9e4b069c38258876c1641f085bf3b87ca207
kiddmetro <igweclintonmatro@gmail.com>
2024-04-21 15:50:25 +0100

    level 3 incomplete

---
+++ 04Excercise/Level3/level3.js
@@ -0,0 +1,48 @@
+// QUESTION 1
+
+const month = prompt('Enter a month');
+const monthLow = month.toLowerCase();
+
+switch(monthLow){
+    case 'january':
+        console.log('January has 31 Days');
+        break;
+    case 'february':
+        console.log('February has 28 Days');
+        break;
+    case 'march':
+        console.log('March has 31 Days');
+        break;
+    case 'april':
+        console.log('April has 30 Days');
+        break;
+    case 'may':
+        console.log('May has 31 Days');
+        break;
+    case 'june':
+        console.log('June has 30 Days');
+        break;
+    case 'july':
+        console.log('July has 31 Days');
+        break;
+    case 'august':
+        console.log('August has 31 Days');
+        break;
+    case 'september':
+        console.log('September has 30 Days');
+        break;
+    case 'october':
+        console.log('October has 31 Days');
+        break;
+    case 'november':
+        console.log('November has 30 Days');
+        break;
+    case 'december':
+        console.log('December has 31 Days');
+        break;
+    default:
+        console.log('Invalid Month');
+}
+
+// QUESTION 2
+
