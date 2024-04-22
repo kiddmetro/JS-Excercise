3113183812a199b049f7271674452a27fb03dcc9
kiddmetro <igweclintonmatro@gmail.com>
2024-04-22 13:03:29 +0100

    initial commit Q14 issue

--- 05Excercise/Level1/level1.js
+++ 05Excercise/Level1/level1.js
@@ -17,43 +17,43 @@ const diffArr = ['34', true, false, 34, 'Esxpae', 5];
 console.log(diffArr.length);
 
 // QUESTION 6
-const itCompaines = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'];
+const itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'];
 
 // QUESTION 7
-console.log(itCompaines);
+console.log(itCompanies);
 
 // QUESTION 8
-console.log(itCompaines.length);
+console.log(itCompanies.length);
 
 // QUESTION 9
-console.log(itCompaines[0]);
-console.log(itCompaines[3]);
-console.log(itCompaines[6]);
+console.log(itCompanies[0]);
+console.log(itCompanies[3]);
+console.log(itCompanies[6]);
 
 // QUESTION 10
-console.log(itCompaines[0]);
-console.log(itCompaines[1]);
-console.log(itCompaines[2]);
-console.log(itCompaines[3]);
-console.log(itCompaines[4]);
-console.log(itCompaines[5]);
-console.log(itCompaines[6]);
+console.log(itCompanies[0]);
+console.log(itCompanies[1]);
+console.log(itCompanies[2]);
+console.log(itCompanies[3]);
+console.log(itCompanies[4]);
+console.log(itCompanies[5]);
+console.log(itCompanies[6]);
 
 // QUESTION 11
-console.log(itCompaines[0].toUpperCase());
-console.log(itCompaines[1].toUpperCase());
-console.log(itCompaines[2].toUpperCase());
-console.log(itCompaines[3].toUpperCase());
-console.log(itCompaines[4].toUpperCase());
-console.log(itCompaines[5].toUpperCase());
-console.log(itCompaines[6].toUpperCase());
+console.log(itCompanies[0].toUpperCase());
+console.log(itCompanies[1].toUpperCase());
+console.log(itCompanies[2].toUpperCase());
+console.log(itCompanies[3].toUpperCase());
+console.log(itCompanies[4].toUpperCase());
+console.log(itCompanies[5].toUpperCase());
+console.log(itCompanies[6].toUpperCase());
 
 // QUESTION 12
-console.log(itCompaines.toString());
+console.log(itCompanies.toString());
 
 // QUESTION 13
 let anyCompany = 'ice';
-if(itCompaines.includes(anyCompany)){
+if(itCompanies.includes(anyCompany)){
     console.log(anyCompany)
 }
 else{
@@ -62,4 +62,54 @@ else{
 
 // OR
 
-// const myCompany 
+const myCompany = 'Amazon';
+const myCompanyIndex = itCompanies.indexOf(myCompany);
+if(myCompanyIndex === -1){
+    console.log(`Company is not Found`);
+}else{
+    console.log(myCompany);
+}
+
+// OR 
+
+let index = itCompanies.indexOf('banana')  
+
+if(index === -1){
+   console.log('This Company does not exist in the array')  
+} else {
+    console.log(index);
+}
+
+// QUESTION 14
+const filteredCompanies = [];
+
+if (itCompanies[0].split('o').length - 1 <= 1) {
+    filteredCompanies.push(itCompanies[0]);
+}
+
+if (itCompanies[1].split('o').length - 1 <= 1) {
+    filteredCompanies.push(itCompanies[1]);
+}
+
+if (itCompanies[2].split('o').length - 1 <= 1) {
+    filteredCompanies.push(itCompanies[2]);
+}
+
+if (itCompanies[3].split('o').length - 1 <= 1) {
+    filteredCompanies.push(itCompanies[3]);
+}
+
+if (itCompanies[4].split('o').length - 1 <= 1) {
+    filteredCompanies.push(itCompanies[4]);
+}
+
+if (itCompanies[5].split('o').length - 1 <= 1) {
+    filteredCompanies.push(itCompanies[5]);
+}
+
+if (itCompanies[6].split('o').length - 1 <= 1) {
+    filteredCompanies.push(itCompanies[6]);
+}
+
+console.log(filteredCompanies);
+
