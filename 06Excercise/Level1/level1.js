479fd4a9d82fd001838a6bda86d4ffa5b368a416
kiddmetro <igweclintonmatro@gmail.com>
2024-04-25 15:09:45 +0100

    question 10 undone

--- 06Excercise/Level1/level1.js
+++ 06Excercise/Level1/level1.js
@@ -41,27 +41,153 @@
 // }
 
 // Define the value of n
-let n = 5;
+// let n = 5;
 
 // Iterate from 0 to n using a for loop
-for (let i = 0; i <= n; i++) {
-    console.log(i);
-}
+// for (let i = 0; i <= n; i++) {
+//     console.log(i);
+// }
 
 // QUESTION 4
 
 // Define the number of rows for the pattern
-const numberOfRows = 7;
+// const numberOfRows = 7;
 
-// Outer loop to iterate over each row
-for (let i = 1; i <= numberOfRows; i++) {
-    let pattern = '';
+// // Outer loop to iterate over each row
+// for (let i = 1; i <= numberOfRows; i++) {
+//     let pattern = '';
 
-    // Inner loop to print '#' characters
-    for (let j = 1; j <= i; j++) {
-        pattern += '#';
-    }
+//     // Inner loop to print '#' characters
+//     for (let j = 1; j <= i; j++) {
+//         pattern += '0';
+//     }
 
-    // Print the pattern for each row
-    console.log(pattern);
+//     // Print the pattern for each row
+//     console.log(pattern);
+// }
+
+
+// QUESTION 5
+
+// let rows = 10;
+
+// for(let i=0; i<=10; i++ ){
+//     console.log(`${i} x ${i} = ${i * i}`);
+// };
+
+// QUESTION 6
+let rows = 10;
+// let exponentOne = 2; 
+// let exponentTwo = 3;
+// for(let i = 0; i <= 10; i++){
+    
+//     console.log(i, Math.pow(i,exponentOne), Math.pow(i,exponentTwo)); 
+// }
+
+// // OR 
+
+// let row = 10;
+// for(let i=0; i<=10; i++ ){
+//         console.log(i, i * i, i * i * i );
+// };
+
+// QUESION 7
+//  let num = 100;
+//  for(let i = 0; i <= num; i++){
+//     let even = i % 2;
+//     if(even === 0){
+//         console.log(i);
+//     }
+//  }
+
+//  QUESTION 8
+// let num = 100;
+// for(let i = 0; i<=num; i++){
+//     let odd = i % 2;
+//     if(odd >= 1){
+//         console.log(i);
+//     }
+// }
+
+// QUESTION 9
+// let num = 100;
+
+
+// for(let i = 0; i <=num ;i++){
+//     let notPrime = false;
+//     for(let j = 2; j <= i; j++){
+//         if( i % j === 0 ){
+//             notPrime = true;
+//         }
+//         if(notPrime === false){
+//             console.log(i)
+//         }
+//     }
+// }
+
+// QUESTION 10
+// let num = 100;
+// let sum = 0;
+// for(let i = 0; i <= num;i++){
+//     sum = sum + i;
+// }
+// console.log(`The sum of all numbers from 0 to 100 is ${sum}`);
+
+// QUESTION 11
+
+// let num = 100;
+// let sumEven = 0;
+// let sumOdd = 0;
+//  for(let i = 0; i <= num; i++){
+//     let even = i % 2;
+//     if(even === 0){
+//         sumEven = sumEven + i;
+//     }
+
+//     let odd = i % 2;
+//         if(odd >= 1){
+//             sumOdd = sumOdd + i;
+//         }
+    
+//  }
+//  console.log(`The sum of all evens from 0 to 100 is ${sumEven}. And the sum of all odds from 0 to 100 is ${sumOdd}.`)
+
+
+// QUESTION 12
+// const newArr = [];
+
+// let num = 100;
+// let sumEven = 0;
+// let sumOdd = 0;
+//  for(let i = 0; i <= num; i++){
+//     let even = i % 2;
+//     if(even === 0){
+//         sumEven = sumEven + i;
+        
+//     }
+
+//     let odd = i % 2;
+//     if(odd >= 1){
+//         sumOdd = sumOdd + i;
+//     }
+    
+//  }
+//  let addOne = newArr.push(sumEven);
+//  let addTwo = newArr.push(sumOdd);
+
+//  console.log(newArr)
+
+
+//  QUESTION 13
+const empArr =[]
+
+let num = 5;
+
+for(let i = 1; i <= num;i++ ){
+    let randomNum = Math.floor(Math.random() * num);
+    let pushNum = empArr.push(randomNum);
 }
+console.log(empArr)
+
+
+// QUESTION 14
