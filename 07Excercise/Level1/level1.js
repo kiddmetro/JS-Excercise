6bfd1feac4f3abd9a6fd1a47088378f7d9c41615
kiddmetro <igweclintonmatro@gmail.com>
2024-04-29 14:28:34 +0100

    initial commit

--- 07Excercise/Level1/level1.js
+++ 07Excercise/Level1/level1.js
@@ -38,3 +38,104 @@ function volumeOfRectPrism(length,width,height,...args){
   return volume;
 }
 console.log(volumeOfRectPrism(4,4,4));
+
+// QUESTION 7
+ areaOfCircle = (r) =>{
+  let area = Math.PI * r * r;
+  return area;
+}
+console.log(areaOfCircle(5));
+
+// QUESTION 8
+circumOfCircle = (r) => {
+  let circum = 2*Math.PI*r;
+  return circum;
+}
+console.log(circumOfCircle(2));
+
+// QUESTION 9
+density = (mass,volume) => {
+  let density = mass/volume;
+  return density;
+}
+console.log(density(10,10));
+
+// QUESTION 10
+function speed(){
+  let distance = 100;
+  let time = 3.5;
+  let speed = distance/time;
+  return speed;
+}
+console.log(speed());
+
+// QUESTION 11
+weight = (mass,gravity) => {
+  let weight = mass * gravity;
+  return weight;
+}
+console.log(weight(10,4));
+
+// QUESTION 12
+function convertCelciustoFahrenheit(oC) {
+  let oF = (oC * 9/5) + 32;
+  return oF;
+}
+console.log(convertCelciustoFahrenheit(34));
+
+// QUESTION 13
+BMI = (weight,height) => {
+  let BMI = weight/(height*height);
+  if(BMI < 18.5){
+    return `Under Weight: BMI is less than 18.5`;
+  }
+  else if(BMI >= 18.5 && BMI <= 24.9){
+    return `Normal Weight: BMI is 18.5 to 24.9`;
+  } 
+  else if(BMI >= 25 && BMI <= 29.9){
+    return `Over Weight: BMI is 25 to 29.9`;
+  }
+  else if(BMI >= 30 && BMI <=43){
+    return `Obese: BMI  is 30 or more`;
+  }
+  else{
+    return `Extremely Fat`;
+  }
+}
+
+console.log(BMI(5940,5));
+
+// QUESTION 14
+checkSeason = (month) => {
+  if(month === 'December' || month === 'January' || month === 'February'){
+    return 'Winter';
+  }
+  else if(month === 'March' || month === 'April' || month === 'May'){
+    return 'Autumn';
+  }
+  else if(month === 'June' || month === 'July' || month === 'August'){
+    return 'Spring';
+  }
+  else if(month === 'September' || month === 'October' || month === 'November'){
+    return 'Autumn';
+  }
+  else {
+    return 'No Month';
+  }
+  
+}
+console.log(checkSeason('October'));
+
+// QUESTION 14
+
+function findMax(...args){
+  let max = args[0];
+  for(i = 0; i <= args.length; i++){
+    if(args[i] > max){
+      max = args[i];
+    }
+  }
+  return max;
+}
+
+console.log(findMax(10,20))
