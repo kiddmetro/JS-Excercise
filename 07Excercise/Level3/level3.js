e9fb74df52a855de12bd65008e920bbed307e100
kiddmetro <igweclintonmatro@gmail.com>
2024-05-06 14:12:10 +0100

    initial commit

--- 07Excercise/Level3/level3.js
+++ 07Excercise/Level3/level3.js
@@ -101,3 +101,14 @@ console.log(arrayOfRgbColors());
 
 
 // QUESTION 5
+
+const hex2rgb = (hex) => {
+    const r = parseInt(hex.slice(1, 3), 16);
+    const g = parseInt(hex.slice(3, 5), 16);
+    const b = parseInt(hex.slice(5, 7), 16);
+
+    return { r, g, b };
+}
+
+console.log(hex2rgb("#d96802"));
+
