fe609aee7afeb9c0199455a13a04e000a166825c
kiddmetro <igweclintonmatro@gmail.com>
2024-05-13 12:36:13 +0100

    initial commit

--- 08Excercise/Level1/level1.js
+++ 08Excercise/Level1/level1.js
@@ -2,3 +2,42 @@
 const dog = {};
 
 // QUESTION 2
+// console.log(dog);
+
+// QUESTION 3
+
+dog.name = 'Rock',
+dog.legs = 4,
+dog.color = 'White',
+dog.age = 23,
+dog.bark = function(){
+    return'woof woof'
+}
+
+console.log(dog);
+
+// QUESTION 4
+let name = dog.name;
+let legs = dog.legs;
+let color = dog.color;
+let age = dog.age;
+let bark = dog.bark();
+
+
+console.log("Name:", name);
+console.log("Legs:", legs);
+console.log("Color:", color);
+console.log("Age:", age);
+console.log("Bark:", bark);
+
+
+// QUESTION 5
+dog.breed = "Golden Retriever";
+
+dog.getDogInfo = function() {
+    return `${this.name} is a ${this.color} ${this.breed} with ${this.legs} legs and is ${this.age} years old. When it barks, it says "${this.bark()}".`;
+};
+
+console.log(dog);
+
+
