4255f4e9abe1d9e1ad14c96dd9d274ab931ce656
kiddmetro <igweclintonmatro@gmail.com>
2024-05-23 23:21:21 +0100

    initial commit

--- 09Excercise/Level1/level1.js
+++ 09Excercise/Level1/level1.js
@@ -1,12 +1,4 @@
-const users = [
-    { name: 'Asabeneh', age: 150 },
-    { name: 'Brook', age: 50 },
-    { name: 'Eyob', age: 100 },
-    { name: 'Elias', age: 22 },
-  ]
-  users.sort((a, b) => {
-    if (a.age < b.age) return -1
-    if (a.age > b.age) return 1
-    return 0
-  })
-  console.log(users);
+// QUESTION 1
+// FOREACH MAP FILTER REDUCE
+
+FOREACH - itrates
