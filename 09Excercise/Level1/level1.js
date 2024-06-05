fac74987aeb76238627448b7e0cf1865978bf1a4
kiddmetro <igweclintonmatro@gmail.com>
2024-06-05 13:35:48 +0100

    initial commit

--- 09Excercise/Level1/level1.js
+++ 09Excercise/Level1/level1.js
@@ -4,7 +4,7 @@
 // FOREACH - iterates through 
 
 
-const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand']
+const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
 const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 const products = [
@@ -15,13 +15,49 @@ const products = [
   { product: 'coffee', price: 10 },
   { product: 'tea', price: '' },
 ]
+
 // QUESTION 2
-function callback(element){
-    return element;
-}
-element.foreach(callback);
-element.map(callback);
-element.filter(callback);
-element.reduce(callback);
+// function callback(element){
+//     return element;
+// }
+// element.foreach(callback);
+// element.map(callback);
+// element.filter(callback);
+// element.reduce(callback);
 
 // QUESTION 3
+// countries.forEach(countries => console.log(countries) )
+
+// // QUESTION 4
+// names.forEach(names => console.log(names));
+
+// // QUESTION 5
+// numbers.forEach(element => console.log(element));
+
+// QUESTION 6
+const countriesUpper = countries.map(element => element.toUpperCase());
+console.log(countriesUpper);
+
+// QUESTION 7
+const countriesLength = countries.map(element => element.length);
+console.log(countriesLength);
+
+// QUESTION 8
+const numbersSquare = numbers.map(element => element * element);
+console.log(numbersSquare);
+
+// QUESTION 9
+const namesUpper = names.map(element => element.toUpperCase());
+console.log(namesUpper);
+
+// QUESTION 10
+const mapPrice = products.map(element => element.price);
+console.log(mapPrice);
+
+// QUESTION 11
+const landFilter = countries.filter(element => element.includes('land'));
+console.log(landFilter);
+
+// QUESTION 12
+const sixFIlter = countries.filter(element => element.length === 6 )
+console.log(sixFIlter)
