7d630f17e21a74309b4550785b69283d6cd1af8e
kiddmetro <igweclintonmatro@gmail.com>
2024-06-15 21:06:20 +0100

    initial commit

--- 09Excercise/Level2/level2.js
+++ 09Excercise/Level2/level2.js
@@ -6,3 +6,8 @@ const products = [
     { product: 'coffee', price: 10 },
     { product: 'tea', price: '' },
   ];
+
+//   QUESTIION 1
+const totalPrice = products.filter(product => typeof product.price === 'number' && product.price > 0).reduce((sum, product) => sum + product.price, 0);
+
+console.log(totalPrice);
