781e6a865843ca10f895d86656512a050fbad6ff
Igwe Clinton <igweclintonmatro@gmail.com>
2024-07-06 16:09:18 +0100

    updated code

--- 09Excercise/Level3/level3.js
+++ 09Excercise/Level3/level3.js
@@ -2,10 +2,26 @@ const countries = require('../Level2/countries_data');
 
 // Excercise 1
 
-  countries.sort((a, b) => {
+ const sortedPopulation = countries.sort((a, b) => {
     if (a.population < b.population) return -1
     if (a.population > b.population) return 1
     return 0
-  })
-  console.log(countries);
+  });
+  console.log(sortedPopulation);
 
+
+  // sorted by name
+  const sortedNames = countries.sort((a,b) => {
+    if (a.name < b.name) return -1
+    if (a.name > b.name) return 1
+    return 0
+  })
+  console.log(sortedNames);
+
+  // sorted by capital
+const sortedCapital = countries.sort((a,b) => {
+    if (a.capital < b.capital) return -1
+    if (a.capital > b.capital) return 1
+    return 0
+  });
+  console.log(sortedCapital);
