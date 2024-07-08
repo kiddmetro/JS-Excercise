e53dc017d8c6d30dc538bc7c06f77407cb9d6924
Igwe Clinton <igweclintonmatro@gmail.com>
2024-07-08 14:41:19 +0100

    initial commit

--- 10Excercise/Level1/level1.js
+++ 10Excercise/Level1/level1.js
@@ -1,3 +1,14 @@
-const a = [4, 5, 8, 9]
-const b = [3, 4, 5, 7]
-const countries = ['Finland', 'Sweden', 'Norway']
+const a = [4, 5, 8, 9];
+const b = [3, 4, 5, 7];
+const countries = ['Finland', 'Sweden', 'Norway'];
+
+// QUESTION 1
+const empty = new Set();
+console.log(empty);
+
+// QUESTION 2
+const set = new Set();
+for (let i = 0; i <= 10; i++) {
+    set.add(i);
+}
+console.log(set);
