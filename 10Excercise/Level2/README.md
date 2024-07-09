c917128b533e49415800201940d77d2c23ac846d
Igwe Clinton <igweclintonmatro@gmail.com>
2024-07-09 16:47:34 +0100

    updated code

---
+++ 10Excercise/Level2/README.md
@@ -0,0 +1,5 @@
+# Exercises:Level 2
+
+1. Find a union b
+2. Find a intersection b
+3. Find a with b
---
+++ 10Excercise/Level2/level2.js
@@ -0,0 +1,17 @@
+const a = [4, 5, 8, 9];
+const b = [3, 4, 5, 7];
+const countries = ['Finland', 'Sweden', 'Norway'];
+
+
+// QUESTION 1
+
+
+
+// QUESTION 2
+const A = new Set(a);
+const B = new Set(b);
+
+const c = [...A].filter(x => B.has(x));
+const C = new Set(c);
+
+console.log(C)
