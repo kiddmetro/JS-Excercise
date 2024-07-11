9f069588c551736ed58f62d34ffe393c9b0a562b
Igwe Clinton <igweclintonmatro@gmail.com>
2024-07-11 15:44:41 +0100

    initial commit

--- 11Excercise/Level1/level1.js
+++ 11Excercise/Level1/level1.js
@@ -1,52 +1,27 @@
-const constants = [2.72, 3.14, 9.81, 37, 100]
-const countries = ['Finland', 'Estonia', 'Sweden', 'Denmark', 'Norway']
+const constants = [2.72, 3.14, 9.81, 37, 100];
+
+// QUESTION 1
+
+const deConstants = [e, pi, gravity, humanBodyTemp, waterBoilingTemp] = constants;
+console.log(deConstants);
+
+// QUESTION 2
+const countries = ['Finland', 'Estonia', 'Sweden', 'Denmark', 'Norway'];
+
+const deCountries = [fin, est, sw, den, nor] = countries;
+console.log(deCountries);
+
+
+
+// QUESTION 3
+
 const rectangle = {
   width: 20,
   height: 10,
   area: 200,
   perimeter: 60
 }
-const users = [
-{
-  name:'Brook',
-  scores:75,
-  skills:['HTM', 'CSS', 'JS'],
-  age:16
-},
-{
-  name:'Alex',
-  scores:80,
-  skills:['HTM', 'CSS', 'JS'],
-  age:18
-},
-{
-  name:'David',
-  scores:75,
-  skills:['HTM', 'CSS'],
-  age:22
-},
-{
-  name:'John',
-  scores:85,
-  skills:['HTML'],
-  age:25
-},
-{
-  name:'Sara',
-  scores:95,
-  skills:['HTM', 'CSS', 'JS'],
-  age: 26
-},
-{
-  name:'Martha',
-  scores:80,
-  skills:['HTM', 'CSS', 'JS'],
-  age:18
-},
-{
-  name:'Thomas',
-  scores:90,
-  skills:['HTM', 'CSS', 'JS'],
-  age:20
-}
-]
+
+let {width, height, area, perimeter} = rectangle;
+console.log(width, height, area, perimeter);
+
