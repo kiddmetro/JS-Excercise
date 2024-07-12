135cdea4f8b64c4588e2819edabbe81cbca770a8
Igwe Clinton <igweclintonmatro@gmail.com>
2024-07-12 14:06:51 +0100

    initial commit

--- 11Excercise/Level3/README.md
+++ 11Excercise/Level3/README.md
@@ -3,3 +3,12 @@
 1. Destructure the countries object print name, capital, population and languages of all countries
 
 2. A junior developer structure student name, skills and score in array of arrays which may not easy to read. Destructure the following array name to name, skills array to skills, scores array to scores, JavaScript score to jsScore and React score to reactScore variable in one line.
+
+3. Write a function called convertArrayToObject which can convert the array to a structure object.
+
+4. Copy the student object to newStudent without mutating the original object. In the new object add the following ?
+
+- Add Bootstrap with level 8 to the front end skill sets
+- Add Express with level 9 to the back end skill sets
+- Add SQL with level 8 to the data base skill sets
+- Add SQL without level to the data science skill sets
--- 11Excercise/Level3/level3.js
+++ 11Excercise/Level3/level3.js
@@ -8,8 +8,68 @@ const countries = require('../../09Excercise/Level2/countries_data');
 // }
 
 // QUESTION 2
-const student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]];
+// const student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]];
 
-const [name, skills, [jsScore, reactScore]] = student;
+// const [name, skills, [jsScore, reactScore]] = student;
 
-console.log(`Name: ${name} | Skills: ${skills} | Js Score: ${jsScore} | React Score: ${reactScore}`)
+// console.log(`Name: ${name} | Skills: ${skills} | Js Score: ${jsScore} | React Score: ${reactScore}`);
+
+// QUESTION 3
+const students = [
+    ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]],
+    ['John', ['HTM', 'CSS', 'JS', 'React'], [85, 80, 85, 80]]
+  ];
+
+  const convertArrayToObject = (array) => {
+    const newArray = [];
+    const newObj = {};
+    for (const arr of array) {
+      const [name, skills, scores] = arr;
+        newObj.name = name;
+        newObj.skills = skills;
+        newObj.scores = scores; 
+      newArray.push(newObj);
+    }
+    return newArray;
+  }
+
+//   console.log(convertArrayToObject(students));
+
+// QUESTION 4
+const student = {
+    name: 'David',
+    age: 25,
+    skills: {
+      frontEnd: [
+        { skill: 'HTML', level: 10 },
+        { skill: 'CSS', level: 8 },
+        { skill: 'JS', level: 8 },
+        { skill: 'React', level: 9 }
+      ],
+      backEnd: [
+        { skill: 'Node',level: 7 },
+        { skill: 'GraphQL', level: 8 },
+      ],
+      dataBase:[
+        { skill: 'MongoDB', level: 7.5 },
+      ],
+      dataScience:['Python', 'R', 'D3.js']
+    }
+}
+
+const newStudent = { ...student };
+newStudent.skills.frontEnd.push({ skill: 'Bootstrap', level: 8 });
+newStudent.skills.backEnd.push({ skill: 'Express', level: 9 });
+newStudent.skills.dataBase.push({ skill: 'SQL', level: 8 });
+newStudent.skills.dataScience.push('SQL');
+
+console.dir(newStudent, { depth: null });
+
+
+
+
+
+
+
+
+
