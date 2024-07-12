4cf6c8daac54524aa1359ae0532adc1ec2caa252
Igwe Clinton <igweclintonmatro@gmail.com>
2024-07-12 12:28:26 +0100

    initial commit

--- 11Excercise/Level3/level3.js
+++ 11Excercise/Level3/level3.js
@@ -2,3 +2,14 @@ const countries = require('../../09Excercise/Level2/countries_data');
 
 // QUESTION 1
 
+// for (const country of countries) {
+//     const{name, capital,population, languages} = country;
+//     console.log(name, capital, population, languages);
+// }
+
+// QUESTION 2
+const student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]];
+
+const [name, skills, [jsScore, reactScore]] = student;
+
+console.log(`Name: ${name} | Skills: ${skills} | Js Score: ${jsScore} | React Score: ${reactScore}`)
