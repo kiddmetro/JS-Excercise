7d9214345903bf121aeee13c177b3a1f7ecfb9eb
Igwe Clinton <igweclintonmatro@gmail.com>
2024-07-13 16:29:28 +0100

    initial commit

--- 12Excercise/Level1/level1.js
+++ 12Excercise/Level1/level1.js
@@ -10,4 +10,20 @@ let sum = 0
 for (let i = 0; i < array.length; i++) {
     sum += array[i]
 }
-console.log(sum)  
+console.log(sum);
+
+// QUESTION 2
+
+let points = ['-1', '2', '-4', '-3', '-1', '0', '4', '8'];
+let numericPoints = points.map(point => {
+  let match = point.match(/-?\d+/);
+  return match ? parseInt(match[0]) : 0;
+});
+let sortedPoints = numericPoints.sort((a, b) => a - b);
+let distance = sortedPoints[sortedPoints.length - 1] - sortedPoints[0];
+
+console.log("Sorted Points: ", sortedPoints);
+console.log("Distance: ", distance);
+
+
+
