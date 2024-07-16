1dd6492c09e6d81d996119671f8d452a48ae4d16
Igwe Clinton <igweclintonmatro@gmail.com>
2024-07-16 11:45:00 +0100

    initial commit

---
+++ 13Excercise/Level2/README.md
@@ -0,0 +1,5 @@
+# Exercises:Level 2
+
+1. 10 > 2 * 10 use console.assert()
+2. Write a warning message using console.warn()
+3. Write an error message using console.error()
---
+++ 13Excercise/Level2/level2.js
@@ -0,0 +1,9 @@
+// QUESTION 1
+console.assert(10 > 2 * 10, '10 is not greater than 2 * 10');
+
+// QUESTION 2
+
+console.warn('This is a warning!');
+
+// QUESTION 3
+console.error('This is a error!');
