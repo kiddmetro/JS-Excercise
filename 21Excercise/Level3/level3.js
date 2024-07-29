36acc0c7333a65a727a5d8cbaa5e042646fa230d
Igwe Clinton <igweclintonmatro@gmail.com>
2024-07-29 11:48:54 +0100

    initial commit

--- 21Excercise/Level3/level3.js
+++ 21Excercise/Level3/level3.js
@@ -4,16 +4,15 @@
 const h1 = document.querySelector('h1');
 const text = h1.textContent;
 const year = '2020';
+h1.style.fontWeight = 'normal';
 
 if (text.includes(year)) {
     const newText = text.replace(year, `<span id="year">${year}</span>`);
-    
-    // Set the innerHTML of the h1 element to the new string
+
     h1.innerHTML = newText;
-    // Select the span containing the year 2020
     const yearSpan = document.getElementById('year');
     
-    yearSpan.style.fontSize = '2em'; 
+    yearSpan.style.fontSize = '1.5em'; 
     yearSpan.style.fontFamily = 'Arial';
 
     function getRandomColors(){
@@ -29,3 +28,24 @@ if (text.includes(year)) {
     }, 1000);
 }
 
+// align items center
+const getDiv = document.getElementsByClassName('wrapper');
+const getBody = document.getElementsByTagName('body')[0];
+
+getBody.style.width = 'auto';
+getBody.style.height = '100vh';
+getBody.style.display = 'flex';
+getBody.style.justifyContent = 'center';
+
+getDiv[0].style.justifyContent = 'center';
+getDiv[0].style.textAlign = 'center';
+
+// edit the h2
+const h2 = document.querySelector('h2');
+
+h2.style.textDecoration = "underline";
+h2.style.fontFamily = 'san serif';
+h2.style.fontSize = '1rem';
+h2.style.fontWeight = 'normal';
+
+// adding date and time
