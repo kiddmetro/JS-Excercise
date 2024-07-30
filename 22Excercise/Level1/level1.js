2fdb46fbf81a5e6a00958bc09f10db9ea730eb01
Igwe Clinton <igweclintonmatro@gmail.com>
2024-07-30 15:14:27 +0100

    initial commit

--- 22Excercise/Level1/level1.js
+++ 22Excercise/Level1/level1.js
@@ -3,15 +3,40 @@ let title
 for (let i = 0; i <= 101; i++) {
     title = document.createElement('p')
     title.className = 'title'
-    title.style.fontSize = '30px'
-    // title.style.display = 'grid'
-    // title.style.backgroundColor = 'black'
-    // title.style.color = 'white'
-    // title.style.padding =  '10px'
-    // title.style.gridTemplateColumns = '1fr 1fr 1fr 1fr 1fr'
+    title.style.fontSize = '60px'
+    title.style.color = '#fff'
     title.textContent = i
-    document.getElementById('#container').appendChild(title)
+    
+    // general styling
+    title.style.margin = '7px'
+    title.style.padding = '40px'
+    title.style.display = 'flex'
+    title.style.justifyContent = 'center'
+    title.style.alignItems = 'center'
+    // background color logic
+    let checker = parseInt(title.textContent)
+    if (checker % 2 === 0 ){
+        title.style.background = 'rgb(0, 194, 0)'
+    }else{
+        title.style.background = 'rgb(255, 221, 0)'
+    }
+
+    function isPrime(num) {
+        if (num <= 1) return false;
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) return false;
+        }
+        return true;
+    }
+    
+    if (isPrime(checker)) {
+        title.style.background = 'rgb(197, 1, 40)'; // Prime numbers
+    }
+
+    document.getElementById('container').appendChild(title)
 }
 
-let getBody = document.getElementsByTagName('body')[0]
+let getDiv = document.getElementById('container')
+getDiv.style.display = 'grid'
+getDiv.style.gridTemplateColumns = '1fr 1fr 1fr 1fr 1fr 1fr'
 
